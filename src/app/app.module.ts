80af2742943fd969e61226da3b8eb2594cfa0e08
Madenityan <marinakorsyn@gmail.com>
2018-12-26 15:15:44 +0200

    test bem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserTableComponent } from './user-table/user-table.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { TestBemComponent } from './test-bem/test-bem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserTableComponent,
-    UserListComponent
+    UserListComponent,
+    TestBemComponent
   ],
   imports: [
     BrowserModule,
